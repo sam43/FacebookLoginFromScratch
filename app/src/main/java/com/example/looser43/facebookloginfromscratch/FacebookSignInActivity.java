bef26f243a9127575b5ce9696118f343b7f62ea9
sam43 <sayemruet.103043@gmail.com>
2017-07-26 01:34:12 +0600

    Initial commit

---
+++ app/src/main/java/com/example/looser43/facebookloginfromscratch/FacebookSignInActivity.java
@@ -0,0 +1,13 @@
+package com.example.looser43.facebookloginfromscratch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FacebookSignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_facebook_sign_in);
+    }
+}
