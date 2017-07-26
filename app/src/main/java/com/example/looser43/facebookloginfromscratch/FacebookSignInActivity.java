f8137cd8d6d92ed653476905c750294d2d791f45
sam43 <scode43@gmail.com>
2017-07-26 12:32:58 +0600

    FB Sign In Done (Basic)

--- app/src/main/java/com/example/looser43/facebookloginfromscratch/FacebookSignInActivity.java
+++ app/src/main/java/com/example/looser43/facebookloginfromscratch/FacebookSignInActivity.java
@@ -1,13 +1,68 @@
 package com.example.looser43.facebookloginfromscratch;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.facebook.login.widget.ProfilePictureView;
+
 
 public class FacebookSignInActivity extends AppCompatActivity {
+
+    CallbackManager callbackManager;
+    TextView greeting;
+    ProfilePictureView profilePictureView;
+    LoginButton loginButton;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+/*        FacebookSdk.sdkInitialize(this.getApplicationContext());
+        AppEventsLogger.activateApp(this);*/
+
+        callbackManager = CallbackManager.Factory.create();
+        // If using in a fragment
+        //loginButton.setFragment(this);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+        callbackManager = CallbackManager.Factory.create();
+
         setContentView(R.layout.activity_facebook_sign_in);
+        greeting = (TextView)findViewById(R.id.info);
+        loginButton = (LoginButton)findViewById(R.id.login_button);
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                greeting.setText("User ID:  " +
+                        loginResult.getAccessToken().getUserId() + "\n" +
+                        "Auth Token: " + loginResult.getAccessToken().getToken());
+            }
+
+            @Override
+            public void onCancel() {
+                greeting.setText("Login attempt cancelled.");
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+                greeting.setText("Login attempt failed.");
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
